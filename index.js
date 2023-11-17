0b0346d73005cb2d8a65a1dc2be2e7cf0b89f0c1
PavelBakhtin <pavelbakhtin88@gmail.com>
2023-11-17 17:25:55 +0200

    7

--- index.js
+++ index.js
@@ -119,13 +119,16 @@ const getFavPosts = async (chatId, query) => {
 };
 const addPost = async (msg, type) => {
   await connectToDb();
-
+  const user = await User.findOne({ telegramId: msg.from.id });
+  console.log(user.number);
+  let nameUser = msg.chat.username || "Користувач";
   try {
     const now = new Date();
-    await connectToDb();
+
     const post = {
       authorId: msg.from.id,
-      name: msg.chat.username,
+      name: nameUser,
+      number: user.number,
       info: msg.text,
       type: type,
       date: date.format(now, "HH:mm:ss DD/MM/YYYY"),
@@ -147,12 +150,9 @@ const deletePost = async (id) => {
 };
 bot.setMyCommands([{ command: "/start", description: "Початок спілкування" }]);
 bot.onText(/\/start/, async (msg) => {
-  connectToDb();
-  const user = await User.findOne({ telegramId: msg.from.id });
-  if (user) {
-    console.log(user);
-  }
   const chatId = msg.chat.id;
+  const user = await User.findOne({ telegramId: msg.from.id });
+
   if (!user) {
     bot.sendMessage(chatId, `Для продовження роботи відправте Ваш номер`, {
       reply_markup: {
@@ -161,14 +161,30 @@ bot.onText(/\/start/, async (msg) => {
       request_contact: true,
     });
     bot.once("message", async (msg) => {
-      console.log(msg)
-      // if (msg.text !== "Назад" && msg.text[0] != "/") {
-      //   bot.sendMessage(chatId, "Номер отримано", {
-      //     reply_markup: { keyboard: keyboard.home },
-      //   });
-      // }
-      return;
+      console.log(msg.contact.phone_number);
+
+      const newUser = {
+        telegramId: msg.from.id,
+        favs: [],
+        number: msg.contact.phone_number,
+      };
+      await new User(newUser).save();
+      const user = await User.findOne({ telegramId: msg.from.id });
+      if (user) {
+        bot.sendMessage(chatId, `Виберіть команду для початку роботи`, {
+          reply_markup: {
+            keyboard: keyboard.home,
+          },
+        });
+      } else return;
     });
+    if (user) {
+      bot.sendMessage(chatId, `Виберіть команду для початку роботи`, {
+        reply_markup: {
+          keyboard: keyboard.home,
+        },
+      });
+    }
 
     // const user = {
     //   telegramId: msg.from.id,
@@ -181,7 +197,7 @@ bot.onText(/\/start/, async (msg) => {
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const user = await bot.getChatMember(chatId, msg.from.id);
-  const userName = user.user.username;
+  // const userName = user.user.username;
   switch (msg.text) {
     case keyboardBtns.home.all:
       bot.sendMessage(chatId, "Всі оголошення", {
@@ -235,18 +251,18 @@ bot.on("message", async (msg) => {
       await getPosts(chatId, { type: "Sell" });
       break;
     case keyboardBtns.home.favs:
-      await getFavPosts(chatId, msg);
       bot.sendMessage(chatId, "Обрані оголошення:", {
         reply_markup: {
           parse_mode: "HTML",
           keyboard: keyboard.favs,
         },
       });
+      await getFavPosts(chatId, msg);
       break;
     case keyboardBtns.home.buy:
-      if (!userName) {
-        console.log("no username");
-      }
+      // if (!userName) {
+      //   console.log("no username");
+      // }
       bot.sendMessage(
         chatId,
         "Введіть власне оголошення про пошук та натисніть відправити!",
@@ -285,13 +301,13 @@ bot.on("message", async (msg) => {
 
       break;
     case keyboardBtns.home.my:
-      await getMyPosts(chatId, { name: msg.from.username });
       bot.sendMessage(chatId, "Мої оголошення", {
         reply_markup: {
           parse_mode: "HTML",
           keyboard: keyboard.my,
         },
       });
+      await getMyPosts(chatId, { number: msg.from.number });
       break;
     case keyboardBtns.back:
       bot.sendMessage(chatId, "Меню", {
@@ -300,15 +316,15 @@ bot.on("message", async (msg) => {
       break;
   }
 });
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
+// bot.onText(/\/start/, (msg) => {
+//   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, `Виберіть команду для початку роботи`, {
-    reply_markup: {
-      keyboard: keyboard.home,
-    },
-  });
-});
+//   bot.sendMessage(chatId, `Виберіть команду для початку роботи`, {
+//     reply_markup: {
+//       keyboard: keyboard.home,
+//     },
+//   });
+// });
 bot.onText(/\/I(.+)/, async (msg, [source, match]) => {
   const id = source.substr(2, source.length);
   const chatId = msg.chat.id;
@@ -332,7 +348,7 @@ bot.onText(/\/I(.+)/, async (msg, [source, match]) => {
           [
             {
               text: "Зв'язатися",
-              url: `t.me/${post.name}`,
+              url: `t.me/+${post.number}`,
             },
             {
               text: "Всі оголошення автора",
@@ -366,7 +382,6 @@ const ACTION_TYPE = {
 const toggleFavPost = async (userId, queryId, data) => {
   try {
     connectToDb();
-
     const user = await User.findOne({ telegramId: userId });
 
     if (!user) {
@@ -394,7 +409,7 @@ const toggleFavPost = async (userId, queryId, data) => {
     console.error(error);
   }
 };
-bot.on("callback_query", (query) => {
+bot.on("callback_query", async (query) => {
   let data;
   try {
     data = JSON.parse(query.data);
@@ -408,22 +423,22 @@ bot.on("callback_query", (query) => {
   const { type } = data;
   switch (type) {
     case ACTION_TYPE.TOGGLE_FAV_POST:
-      toggleFavPost(userId, query.id, data);
-      setTimeout(() => {
-        bot.sendMessage(chatId, "Обрані оголошення:", {
-          reply_markup: {
-            parse_mode: "HTML",
-            keyboard: keyboard.favs,
-          },
-        });
-        getFavPosts(chatId, query);
-      }, 1000);
+      await toggleFavPost(userId, query.id, data);
+
+      bot.sendMessage(chatId, "Обрані оголошення:", {
+        reply_markup: {
+          parse_mode: "HTML",
+          keyboard: keyboard.favs,
+        },
+      });
+      await getFavPosts(chatId, query);
+
       break;
     case ACTION_TYPE.GET_POSTS_BY_USER:
       bot.sendMessage(chatId, "Всі оголошення автора", {
         reply_markup: { keyboard: keyboard.back },
       });
-      getPosts(chatId, { authorId: data.authorId });
+      await getPosts(chatId, { authorId: data.authorId });
 
       break;
     case ACTION_TYPE.DELETE_POST:
@@ -432,12 +447,10 @@ bot.on("callback_query", (query) => {
         bot.sendMessage(chatId, "Оголошеня успішно видалене!", {
           reply_markup: { keyboard: keyboard.my },
         });
-      setTimeout(() => {
-        bot.sendMessage(chatId, "Мої оголошення", {
-          reply_markup: { keyboard: keyboard.my },
-        });
-        getMyPosts(chatId, { name: query.message.chat.username });
-      }, 1000);
+      bot.sendMessage(chatId, "Мої оголошення", {
+        reply_markup: { keyboard: keyboard.my },
+      });
+      getMyPosts(chatId, { number: query.message.chat.number });
 
       break;
   }
--- models/post.js
+++ models/post.js
@@ -1,24 +1,28 @@
 const { Schema, models, model } = require("mongoose");
 
 const PostSchema = new Schema({
-	creator: {
-		type: Schema.Types.ObjectId,
-		ref: "User",
-	},
-	authorId: { type: Number, requires: [true] },
-	name: { type: String, requires: [true] },
-	info: {
-		type: String,
-		requires: [true],
-	},
-	date: {
-		type: String,
-		requires: [true],
-	},
-	type: {
-		type: String,
-		requires: [true],
-	},
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  authorId: { type: Number, requires: [true] },
+  name: { type: String, requires: [false] },
+  info: {
+    type: String,
+    requires: [true],
+  },
+  number: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: String,
+    requires: [true],
+  },
+  type: {
+    type: String,
+    requires: [true],
+  },
 });
 const Post = models.Post || model("Post", PostSchema);
 module.exports = Post;
